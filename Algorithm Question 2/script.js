fd6588bb65b396b131dcce63f6d894fc0c66bd6b
Şafak Kaya <108135774+SafakKayaa@users.noreply.github.com>
2023-12-16 08:30:06 +0100

    Update: another similar question is added

--- Algorithm Question 2/script.js
+++ Algorithm Question 2/script.js
@@ -1,12 +1,6 @@
 // ! Write a function that takes a string as input and
 //! returns the string reversed.
 
-const originalString = "Hello, React Developer!";
-const reversedString = reverseString(originalString);
-console.log(reversedString);
-// Output: "!repoleveD tcareR ,olleH"
-
-
 function reverseString(string) {
     const characters = string.split('');
     const reversedCharacters = characters.reverse();
@@ -14,3 +8,30 @@ function reverseString(string) {
 
     return reversedString;
 }
+
+const originalString = "Hello, React Developer!";
+const reversedString = reverseString(originalString);
+console.log(reversedString);
+// Output: "!repoleveD tcareR ,olleH"
+
+
+
+//? Another Question: Check for Palindrome
+
+//! Write a function that takes a string as input 
+//! and returns true if the string is a palindrome 
+//! (reads the same backward as forward), and false otherwise.
+
+function isPalindrome(str) {
+    const characters = str.split('');
+    const reversedCharacters = characters.reverse();
+    const reversedString = reversedCharacters.join('');
+
+    return str === reversedString;
+}
+
+const str1 = "radar";
+const str2 = "hello";
+
+console.log(isPalindrome(str1)); // Output: true
+console.log(isPalindrome(str2)); // Output: false
